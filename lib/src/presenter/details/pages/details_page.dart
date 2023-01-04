85c4b7e0713d0d5a60cc499349a654ce2f6d4bf0
Pedro Bandeira <pedrobandeira.dev@gmail.com>
2023-01-04 14:49:35 -0400

    Update details_page.dart
    Setting up components over the page

--- lib/src/presenter/details/pages/details_page.dart
+++ lib/src/presenter/details/pages/details_page.dart
@@ -16,11 +16,133 @@ class _DetailsPageState extends State<DetailsPage> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             _GameThumbnail(),
+            _GameMainInfo(
+              information: {
+                'Genre': 'Shooter',
+                'Released on': '25/10/1991',
+                'Publisher': 'Activision Blizzard',
+                'Developed by': 'Infinity Ward',
+              },
+            ),
+            _GameDetailsInfo(),
             _GallerySection(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _GameMainInfo extends StatelessWidget {
+  final Map<String, String> information;
+
+  const _GameMainInfo({
+    required this.information,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      fit: FlexFit.loose,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 24.0,
+            ),
+            child: Text(
+              'Call of Duty Warzone',
+              style: const TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          GridView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 300,
+              mainAxisExtent: 60,
+              crossAxisSpacing: 10,
+            ),
+            itemCount: 4,
+            itemBuilder: (context, index) => _GameInfoItem(
+              title: information.keys.elementAt(index),
+              description: information.values.elementAt(index),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _GameInfoItem extends StatelessWidget {
+  final String title;
+  final String description;
+
+  const _GameInfoItem({
+    Key? key,
+    required this.title,
+    required this.description,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.grey.shade500,
+            ),
+          ),
+          Text(
+            description,
+            style: const TextStyle(
+              fontSize: 16.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _GameDetailsInfo extends StatelessWidget {
+  const _GameDetailsInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: 12.0,
+            ),
+            child: Text(
+              'About Call of Duty: Warzone',
+              style: const TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Text(
+            'Call of Duty: Warzone is both a standalone free-to-play battle royale and modes accessible via Call of Duty: Modern Warfare. Warzone features two modes — the general 150-player battle royle, and “Plunder”. The latter mode is described as a “race to deposit the most Cash”. In both modes players can both earn and loot cash to be used when purchasing in-match equipment, field upgrades, and more. Both cash and XP are earned in a variety of ways, including completing contracts.\r\n\r\nAn interesting feature of the game is one that allows players who have been killed in a match to rejoin it by winning a 1v1 match against other felled players in the Gulag.\r\n\r\nOf course, being a battle royale, the game does offer a battle pass. The pass offers players new weapons, playable characters, Call of Duty points, blueprints, and more. Players can also earn plenty of new items by completing objectives offered with the pass.',
+            textAlign: TextAlign.justify,
+          ),
+        ],
       ),
     );
   }
@@ -33,10 +155,13 @@ class _GameThumbnail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CachedNetworkImage(
-      imageUrl: 'https://www.freetogame.com/g/452/thumbnail.jpg',
-      placeholder: (context, url) => Image.asset(
-        Assets.defaultImage,
+    return Padding(
+      padding: const EdgeInsets.only(top: 32.0),
+      child: CachedNetworkImage(
+        imageUrl: 'https://www.freetogame.com/g/452/thumbnail.jpg',
+        placeholder: (context, url) => Image.asset(
+          Assets.defaultImage,
+        ),
       ),
     );
   }
@@ -57,7 +182,7 @@ class _GallerySection extends StatelessWidget {
           child: Text(
             'Gallery',
             style: const TextStyle(
-              fontSize: 24.0,
+              fontSize: 22.0,
               fontWeight: FontWeight.bold,
             ),
           ),
