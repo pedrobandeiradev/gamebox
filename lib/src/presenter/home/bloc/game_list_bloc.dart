4c8decebabfdce6406172bf32d8fbc9c34b587d8
Pedro Bandeira <pedrobandeira.dev@gmail.com>
2023-01-13 00:49:51 -0400

    Update game_list_bloc.dart
    Adding temporary games list

--- lib/src/presenter/home/bloc/game_list_bloc.dart
+++ lib/src/presenter/home/bloc/game_list_bloc.dart
@@ -5,7 +5,14 @@ import 'package:gamebox/src/domain/usecases/games_list/repository/games_list_rep
 import 'game_list_index.dart';
 
 class GameListBloc extends Bloc {
-  final GameItemEntity game = GameItemEntity(
+  //MARK - Remove
+  late List<GameItemEntity> games = [
+    _game,
+    _game,
+    _game2,
+  ];
+
+  final GameItemEntity _game = GameItemEntity(
     id: 452,
     title: 'Call Of Duty: Warzone',
     shortDescription:
@@ -13,6 +20,16 @@ class GameListBloc extends Bloc {
     thumbnail: 'https://www.freetogame.com/g/452/thumbnail.jpg',
     publisher: 'Activision Blizzard',
     releaseDate: '25/10/1991',
+  );
+
+  final GameItemEntity _game2 = GameItemEntity(
+    id: 452,
+    title: 'AAA Of Duty: 2',
+    shortDescription:
+        'A standalone free-to-play battle royale and modes accessible via Call of Duty: Modern Warfare.',
+    thumbnail: 'https://www.freetogame.com/g/452/thumbnail.jpg',
+    publisher: 'Activision Blizzard',
+    releaseDate: '25/10/1996',
   );
 
   final GamesList repository;
