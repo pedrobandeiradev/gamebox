4287acf378c9aa3cd998400589f43d6ad7eec3bf
Pedro Bandeira <pedrobandeira.dev@gmail.com>
2023-01-13 00:51:11 -0400

    Create home_widgets_index.dart
    Exporting new components

---
+++ lib/src/presenter/home/widgets/home_widgets_index.dart
@@ -0,0 +1,3 @@
+export 'package:gamebox/src/presenter/home/widgets/content_card.dart';
+export 'package:gamebox/src/presenter/home/widgets/search_bar.dart';
+export 'package:gamebox/src/presenter/home/widgets/sorting_options.dart';
